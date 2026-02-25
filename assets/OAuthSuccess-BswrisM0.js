a0f708f5319a6b684227aab0b70b2c90be9f9e4e
ChromaVesper <amr803056@gmail.com>
2026-02-25 11:59:23 +0530

    Updates

---
+++ assets/OAuthSuccess-BswrisM0.js
@@ -0,0 +1,1 @@
+import{u as a,l as o,r as n,j as r}from"./index-C2IzmjIH.js";function i(){const e=a(),[s]=o();return n.useEffect(()=>{const t=s.get("token");t?(localStorage.setItem("token",t),e("/")):e("/login")},[]),r.jsx("div",{children:"Logging you in..."})}export{i as default};
